48adf9febea81ec6ca16bbeb211931b845ec24a6
SwastikGiri <swastikgiri92@gmail.com>
2025-04-07 16:03:10 +0530

    TransactionPanelUpdated

--- src/containers/TransactionPage/TransactionPanel/TransactionPanel.js
+++ src/containers/TransactionPage/TransactionPanel/TransactionPanel.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import classNames from 'classnames';
-import PetProfileForm from './PetProfileForm'; // adjust the path if needed
+import PetProfileForm from '../../../components/PetProfile/PetProfileForm'; // adjust the path if needed
 
 import { FormattedMessage, injectIntl, intlShape } from '../../../util/reactIntl';
 import { displayPrice } from '../../../util/configHelpers';
